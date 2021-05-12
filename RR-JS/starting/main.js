83b3cc84cd3dc6f12e833d58d8b80a5dd27d9c3f
marychungg <qqyinty@gmail.com>
2021-05-12 12:40:50 +0800

    updated RR-JS

--- RR-JS/starting/main.js
+++ RR-JS/starting/main.js
@@ -23,15 +23,15 @@ class Field {
   winOrLose () {
       let x = this.x;
       let y = this.y;
-      if (x >=0 && x < this._field[0].length && y >=0 && y < this._field.length) {
-        if (this._field[x][y] === hole) {
+      if (x >=0 && x < this.field[0].length && y >=0 && y < this.field.length) {
+        if (this.field[x][y] === hole) {
           console.log("You Lost!");
           return true;
-        } else if(this._field[x][y] === hat) {
+        } else if(this.field[x][y] === hat) {
           console.log("Congrats! You found your hat!");
           return true;
         } else {
-            this._field[x][y] = pathCharacter;
+            this.field[x][y] = pathCharacter;
             return false;
       } 
     }
